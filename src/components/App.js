99c6e66b24c81aa8ff9a3692a8769acd9651cac5
Anastasiya Fedotova <nastenaf@list.ru>
2022-07-21 17:53:39 +0300

    react 11Пр

--- src/components/App.js
+++ src/components/App.js
@@ -56,7 +56,8 @@ function App({ isOpen }) {
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
       setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
-    });
+    })
+    .catch((err) => console.log(err));
   }
 
   function handleUpdateUser(newUser) {
